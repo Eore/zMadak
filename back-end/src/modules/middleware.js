2c01945c89b2f85a4e8e9efd1cc3b24746f24392
Eore <eore26@gmail.com>
2018-07-13 14:47:50 +0700

    tambah cors

--- back-end/src/modules/middleware.js
+++ back-end/src/modules/middleware.js
@@ -1,7 +1,9 @@
 const { json, urlencoded } = require("body-parser");
 const cookieParser = require("cookie-parser");
+const cors = require("cors");
 
 module.exports = app => {
+  app.use(cors());
   app.use(cookieParser("miawmiaw"));
   app.use(json());
   app.use(urlencoded({ extended: false }));
